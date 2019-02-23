bca64bca27e4b1a3ac294e270ecb8c056ecaa5fa
murai <wawawa21071414@gmail.com>
2019-02-23 04:27:44 +0000

    bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(status: 'テスト 1', content: 'テストコンテント 1')
+
+
+
+(1..100).each do |number|
+  Task.create(status: 'テストステータス' + number.to_s, content: 'テスト ' + number.to_s)
+end
